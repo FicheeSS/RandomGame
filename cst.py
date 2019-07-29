1987f5bd34abf6b5771e780d794ee3a9a792e684
FicheeSS <theo.ange.bocquet@gmail.com>
2019-07-29 11:35:34 +0200

    image cannot load

--- cst.py
+++ cst.py
@@ -1,11 +1,11 @@
 import pygame
 
-imgsautDroite = pygame.image.load("sautDroite.bmp")
-imgsautGauche = pygame.image.load("sautGauche.bmp")
-imgstill = pygame.image.load("still.bmp")
-imgmarcheDroite = pygame.image.load("marcheDroite.bmp")
-imgmarcheGauche = pygame.image.load("marcheGauche.bmp")
-imggrass = pygame.image.load("grass.bmp")
+imgsautDroite = pygame.image.load("./img/sautDroite.bmp")
+imgsautGauche = pygame.image.load("./img/sautGauche.bmp")
+imgstill = pygame.image.load("./img/still.bmp")
+imgmarcheDroite = pygame.image.load("./img/marcheDroite.bmp")
+imgmarcheGauche = pygame.image.load("./img/marcheGauche.bmp")
+imggrass = pygame.image.load("./img/grass.bmp")
 
 screenResolution = (1440,900)
 
